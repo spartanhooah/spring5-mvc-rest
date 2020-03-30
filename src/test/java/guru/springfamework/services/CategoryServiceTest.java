26279b77d483f7a7cae47fec1665eb9d473024f3
Ben <spartanhooah@gmail.com>
2020-03-29 19:31:02 -0500

    feature: create CategoryService

---
+++ src/test/java/guru/springfamework/services/CategoryServiceTest.java
@@ -0,0 +1,58 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CategoryServiceTest {
+    private static final Long ID = 2L;
+    private static final String NAME = "Jimmy";
+    private CategoryService categoryService;
+
+    @Mock CategoryRepository categoryRepository;
+    @Mock CategoryMapper categoryMapper;
+
+    @Before
+    public void setUp() {
+        categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+    }
+
+    @Test
+    public void getAllCategories() {
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        List<CategoryDTO> categoryDTOs = categoryService.getAllCategories();
+
+        assertThat(categoryDTOs.size(), is(3));
+    }
+
+    @Test
+    public void getCategoryByName() {
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+        assertThat(categoryDTO.getId(), is(ID));
+        assertThat(categoryDTO.getName(), is(NAME));
+    }
+}
